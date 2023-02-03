46d2c2ee3feff1e32f61f2e363753c7d27c73585
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2023-02-03 16:12:15 -0300

    update header

--- FRONT/src/components/shared/header/header.js
+++ FRONT/src/components/shared/header/header.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 export const Header = () => (
   <div className='flex items-center flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-75'>
-    <input
-      className='flex items-center h-10 px-4 text-sm bg-gray-200 rounded-full focus:outline-none focus:ring'
-      type='search'
-      placeholder='Search'
-    />
+    <div className='my-6'>
+      <h1 className='text-2xl font-bold'>Kanban</h1>
+    </div>
   </div>
 );
