9c1494298b2bb15135c6f5da9befdd6061eb6491
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2023-02-03 16:12:08 -0300

    update main

--- FRONT/src/containers/main/main.js
+++ FRONT/src/containers/main/main.js
@@ -6,15 +6,16 @@ import { Loading } from 'components/shared';
 export const MainContainer = () => {
   const { token, loading } = useSelector(state => state.auth);
   if (loading === 'loading') <Loading />
+  
+  const renderContent = () => {
+    if (!token) return <LoginModal />
+    return <Board />
+  }
 
   return (
     <div className='flex flex-col w-screen h-screen overflow-auto text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200'>
       <Header />
-      <div className='px-10 mt-6'>
-        <h1 className='text-2xl font-bold'>Team Project Board</h1>
-      </div>
-      {token && <Board />}
-      {!token && <LoginModal />}
+      {renderContent()}
     </div>
   );
 };
