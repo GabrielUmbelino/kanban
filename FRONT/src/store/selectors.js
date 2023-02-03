18472e0f4e26157162bf9d97abc504abdfdb9609
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2023-02-03 16:11:40 -0300

    order cards

--- FRONT/src/store/selectors.js
+++ FRONT/src/store/selectors.js
@@ -1,13 +1,46 @@
 export const getTodoCards = state => {
-  if (!state.cards?.data?.length) return []
-  return state.cards?.data?.filter(card => card.lista === 'todo');
+  if (!state.cards?.data?.length) return [];
+  const cards = state.cards?.data?.filter(card => card.lista === 'todo');
+  const orderedCards = cards?.sort((a, b) => {
+    if (a.ordem < b.ordem) {
+      return -1;
+    }
+    if (a.ordem > b.ordem) {
+      return 1;
+    }
+
+    return 0;
+  });
+  return orderedCards;
 };
 export const getDoingCards = state => {
-  if (!state.cards?.data?.length) return []
-  return state.cards?.data?.filter(card => card.lista === 'doing');
+  if (!state.cards?.data?.length) return [];
+  const cards = state.cards?.data?.filter(card => card.lista === 'doing');
+  const orderedCards = cards?.sort((a, b) => {
+    if (a.ordem < b.ordem) {
+      return -1;
+    }
+    if (a.ordem > b.ordem) {
+      return 1;
+    }
+
+    return 0;
+  });
+  return orderedCards;
 };
 
 export const getDoneCards = state => {
-  if (!state.cards?.data?.length) return []
-  return state.cards?.data?.filter(card => card.lista === 'doing');
+  if (!state.cards?.data?.length) return [];
+  const cards = state.cards?.data?.filter(card => card.lista === 'done');
+  const orderedCards = cards?.sort((a, b) => {
+    if (a.ordem < b.ordem) {
+      return -1;
+    }
+    if (a.ordem > b.ordem) {
+      return 1;
+    }
+
+    return 0;
+  });
+  return orderedCards;
 };
